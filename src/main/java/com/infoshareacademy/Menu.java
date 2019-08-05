3cc1d035b085fa11f1361f7dea61c2e94eeb4371
Mikołaj Krawczak <mikolak25@gmail.com>
2019-08-05 21:49:34 +0200

    Added menu, problem with wrong values

--- src/main/java/com/infoshareacademy/Menu.java
+++ src/main/java/com/infoshareacademy/Menu.java
@@ -7,16 +7,81 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Co chcesz zrobic ? \n 1- Przegladanie zbiorow \n 2- Rezerwacja pozycji/anulowanie rezerwacji");
         String choice = scanner.nextLine();
-        if (choice=="1"){
+
+        if (choice.equals("1")){
             browsingCollections();
 
-        } else if(choice=="2"){
+        } else if(choice.equals("2")){
+            reservations();
+        } else {
+            System.out.println("Podaj wartosc 1 lub 2 !!");
+            menu();
         }
     }
 
     public void browsingCollections(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println(" 1- Lista ksiazek \n 2-Wyszukiwanie ksiazek (wg autora, tytulu, wersji audio). \n 3- Przegladanie pojedynczej pozycji \n 9- powrot");
+        String choice = scanner.nextLine();
 
+        switch (choice) {
+            case "1":
+                //zamienic na klase generujaca liste ksiazek
+                System.out.println("Lista ksiazek");
+                break;
+            case "2":
+                //zamienic na klase wyszukujaca ksiazke
+                System.out.println("Wyszukiwanie ksiazek");
+                break;
+            case "3":
+                //zamienic na klase dajaca mozliwosc przegladanie pojedynczej pozycji
+                System.out.println("Przegladanie pojedynczej pozycji");
+                break;
+            case "9":
+                menu();
+            default:
+                System.out.println("Podaj wlasciwa wartosc !");
+                browsingCollections();
+                break;
+        }
+    }
 
+    public String reservations(){
+        //trzeba bedzie ogarnac jakos polaczenie z rezerwacjami albo anulowaniem
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Podaj Imie i Nazwisko     (9- powrot do poczatku)");
+            String name = scanner.nextLine();
+
+            if (name.length()<3){
+                System.out.println("Podaj prawidlowe dane !");
+                reservations();
+            }
+
+        if (name.equals("9")){
+            menu();
+        } else {
+            System.out.println(" 1- rezerwacja \n 2- anulowanie rezerwacji \n 9- powrot do poczatku");
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println(name + " rezerwacja");
+                    break;
+
+                case "2":
+                    System.out.println(name + " anulowanie rezerwacji");
+                    break;
+
+                case "9":
+                    menu();
+                    break;
+
+                    default:
+                        System.out.println("Podaj prawidlowe wartosci !");
+                        reservations();
+                        break;
+                }
+        }
+        return null;
     }
 }
