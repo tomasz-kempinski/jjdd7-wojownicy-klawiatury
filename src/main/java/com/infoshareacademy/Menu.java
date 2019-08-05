c0b2c4f6f6c754631e6fb6459307e3ffacb78c1e
Mikołaj Krawczak <mikolak25@gmail.com>
2019-08-05 19:23:06 +0200

    Modified in Menu

--- src/main/java/com/infoshareacademy/Menu.java
+++ src/main/java/com/infoshareacademy/Menu.java
@@ -8,14 +8,15 @@ public class Menu {
         System.out.println("Co chcesz zrobic ? \n 1- Przegladanie zbiorow \n 2- Rezerwacja pozycji/anulowanie rezerwacji");
         String choice = scanner.nextLine();
         if (choice=="1"){
-            System.out.println("Jak chcesz wyszukac ksiazke? \n 1- Lista ksiazek dostepnych w bazie \n 2- Wyszukiwanie ksiazek wg: autora, tytulu, wersji audio ");
-            choice = scanner.nextLine();
-            //teraz trzeba wywolac odpowiednia klase/metode ktora utworzymy pod te rzeczy
+            browsingCollections();
+
         } else if(choice=="2"){
-            System.out.println("1- Rezerwacja pozycji \n 2-anulowanie pozycji");
-            choice = scanner.nextLine();
-
-
         }
+    }
+
+    public void browsingCollections(){
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
