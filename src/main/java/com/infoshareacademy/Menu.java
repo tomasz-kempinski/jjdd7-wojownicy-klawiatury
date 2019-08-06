7b534d904b47eaf774c147641b01f773bf226397
Mikołaj Krawczak <mikolak25@gmail.com>
2019-08-06 16:24:36 +0200

    Menu in last version

--- src/main/java/com/infoshareacademy/Menu.java
+++ src/main/java/com/infoshareacademy/Menu.java
@@ -2,25 +2,30 @@ package com.infoshareacademy;
 
 import java.util.Scanner;
 
-public class Menu {
-    public void menu(){
+class Menu {
+    void menu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Co chcesz zrobic ? \n 1- Przegladanie zbiorow \n 2- Rezerwacja pozycji/anulowanie rezerwacji \n 9- Zakoncz program");
         String choice = scanner.nextLine();
 
-        if (choice.equals("1")){
-            browsingCollections();
+        switch (choice) {
+            case "1":
+                browsingCollections();
 
-        } else if(choice.equals("2")){
-            reservations();
-        } else if (choice.equals("9")){
-        } else {
-            System.out.println("Podaj wartosc 1, 2 lub 9!!");
-            menu();
+                break;
+            case "2":
+                reservations();
+                break;
+            case "9":
+                break;
+            default:
+                System.out.println("Podaj wartosc 1, 2 lub 9!!");
+                menu();
+                break;
         }
     }
 
-    public void browsingCollections(){
+    private void browsingCollections() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(" 1- Lista ksiazek \n 2- Wyszukiwanie ksiazek (wg autora, tytulu, wersji audio). \n 3- Przegladanie pojedynczej pozycji \n 9- powrot");
         String choice = scanner.nextLine();
@@ -47,50 +52,44 @@ public class Menu {
                 break;
 
         }
-        return;
     }
 
-    public String reservations(){
+    private String reservations() {
         //trzeba bedzie ogarnac jakos polaczenie z rezerwacjami albo anulowaniem
         Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Podaj Imie i Nazwisko     (9- powrot do poczatku)");
-            String name = scanner.nextLine();
+        System.out.println("Podaj Imie i Nazwisko     (9- powrot do poczatku)");
+        String name = scanner.nextLine();
 
-            if (name.equals("9")){
-                menu();
-                return null;
-            }else if (name.length()<3){
-                System.out.println("Podaj prawidlowe dane !");
-                reservations();
-                return null;
-            }
-
-        if (name.equals("9")){
+        if (name.equals("9")) {
             menu();
+            return null;
+        } else if (name.length() < 3) {
+            System.out.println("Podaj prawidlowe dane !");
+            reservations();
+            return null;
+        }
 
-        } else {
-            System.out.println(" 1- rezerwacja \n 2- anulowanie rezerwacji \n 9- powrot do poczatku");
-            String choice = scanner.nextLine();
-            switch (choice) {
-                case "1":
-                    System.out.println(name + " rezerwacja");
+        System.out.println(" 1- rezerwacja \n 2- anulowanie rezerwacji \n 9- powrot do poczatku");
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "1":
+                System.out.println(name + " rezerwacja");
 
-                    return name;
+                return name;
 
-                case "2":
-                    System.out.println(name + " anulowanie rezerwacji");
-                    return name;
+            case "2":
+                System.out.println(name + " anulowanie rezerwacji");
+                return name;
 
-                case "9":
-                    menu();
-                    break;
+            case "9":
+                menu();
+                break;
 
-                    default:
-                        System.out.println("Podaj prawidlowe wartosci !");
-                        reservations();
-                        break;
-                }
+            default:
+                System.out.println("Podaj prawidlowe wartosci !");
+                reservations();
+                break;
         }
         return null;
     }
