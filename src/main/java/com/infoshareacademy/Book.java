97290656d92bf6c31d63a4ca03d3c01a541a1054
Robert Lis <lisrobert0023@gmail.com>
2019-08-06 15:00:28 +0200

    Updated Book class

--- src/main/java/com/infoshareacademy/Book.java
+++ src/main/java/com/infoshareacademy/Book.java
@@ -1,61 +1,60 @@
 package com.infoshareacademy;
 
 public class Book {
-    private final String kind;
-    private final String fullSortKey;
-    private final String title;
-    private final String url;
-    private final String coverColor;
-    private final String epoch;
-    private final String href;
-    private final String hasAudio;
-    private final String genre;
+    private String kind;
+    private String title;
+    private String hasAudio;
+    private String genre;
+    private String author;
 
-    public Book(String kind, String fullSortKey, String title, String url, String coverColor, String epoch, String href, String hasAudio, String genre) {
+    public Book() {
+    }
+
+    public Book(String kind, String title, String hasAudio, String genre, String author) {
         this.kind = kind;
-        this.fullSortKey = fullSortKey;
         this.title = title;
-        this.url = url;
-        this.coverColor = coverColor;
-        this.epoch = epoch;
-        this.href = href;
         this.hasAudio = hasAudio;
         this.genre = genre;
+        this.author = author;
     }
 
     public String getKind() {
         return kind;
     }
 
-    public String getFullSortKey() {
-        return fullSortKey;
+    public void setKind(String kind) {
+        this.kind = kind;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getCoverColor() {
-        return coverColor;
-    }
-
-    public String getEpoch() {
-        return epoch;
-    }
-
-    public String getHref() {
-        return href;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getHasAudio() {
         return hasAudio;
     }
 
+    public void setHasAudio(String hasAudio) {
+        this.hasAudio = hasAudio;
+    }
+
     public String getGenre() {
         return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
