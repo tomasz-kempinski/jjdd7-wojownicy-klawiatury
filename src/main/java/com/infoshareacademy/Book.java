5b1e48029e91e0cb129a361cd82973f096ad7b12
Kacper Ruciński <kacper.rucinski99@outlook.com>
2019-08-08 16:57:20 +0200

    +Update Book.java +Add Parser.Parser.java

--- src/main/java/com/infoshareacademy/Book.java
+++ src/main/java/com/infoshareacademy/Book.java
@@ -1,62 +1,103 @@
 package com.infoshareacademy;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "kind",
+        "title",
+        "author",
+        "has_audio",
+        "genre",
+})
 public class Book {
+
+    @JsonProperty("kind")
     private String kind;
+    @JsonProperty("title")
     private String title;
-    private boolean hasAudio;
-    private List<String> genre;
+    @JsonProperty("author")
     private String author;
+    @JsonProperty("has_audio")
+    private Boolean hasAudio;
+    @JsonProperty("genre")
+    private String genre;
 
-    public Book() {
-    }
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    public Book(String kind, String title, boolean hasAudio, List<String> genre, String author) {
-        this.kind = kind;
-        this.title = title;
-        this.hasAudio = hasAudio;
-        this.genre = genre;
-        this.author = author;
-    }
-
+    @JsonProperty("kind")
     public String getKind() {
         return kind;
     }
 
+    @JsonProperty("kind")
     public void setKind(String kind) {
         this.kind = kind;
     }
 
+    @JsonProperty("title")
     public String getTitle() {
         return title;
     }
 
+    @JsonProperty("title")
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public boolean isHasAudio() {
-        return hasAudio;
-    }
-
-    public void setHasAudio(boolean hasAudio) {
-        this.hasAudio = hasAudio;
-    }
-
-    public List<String> getGenre() {
-        return genre;
-    }
-
-    public void setGenre(List<String> genre) {
-        this.genre = genre;
-    }
-
+    @JsonProperty("author")
     public String getAuthor() {
         return author;
     }
 
+    @JsonProperty("author")
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    @JsonProperty("has_audio")
+    public Boolean getHasAudio() {
+        return hasAudio;
+    }
+
+    @JsonProperty("has_audio")
+    public void setHasAudio(Boolean hasAudio) {
+        this.hasAudio = hasAudio;
+    }
+
+    @JsonProperty("genre")
+    public String getGenre() {
+        return genre;
+    }
+
+    @JsonProperty("genre")
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "kind='" + kind + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", hasAudio=" + hasAudio +
+                ", genre='" + genre + '\'' +
+                ", additionalProperties=" + additionalProperties +
+                '}';
     }
 }
