31f7bbf3b4f01e14f5721050c45fe98774afd6e0
Kacper Ruciński <kacper.rucinski99@outlook.com>
2019-08-09 15:44:39 +0200

    +Parser class created to read data from json file and Book class updated

---
+++ src/main/java/com/infoshareacademy/Parser.java
@@ -0,0 +1,24 @@
+package com.infoshareacademy;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@JsonIgnoreProperties(value = {"full_sort_key", "url", "cover_color", "cover", "epoch", "href", "simple_thumb", "slug", "cover_thumb", "liked"})
+class Parser {
+    public void parseFileToObjects(String file) {
+        ObjectMapper mapper = new ObjectMapper ( );
+        List<Book> books = null;
+        try {
+            books = mapper.readValue (new File (file), new TypeReference<List<Book>> ( ) {
+            });
+        } catch (IOException e) {
+            System.out.println ("Problem with IO occured");
+        }
+        System.out.println (books);
+    }
+}
