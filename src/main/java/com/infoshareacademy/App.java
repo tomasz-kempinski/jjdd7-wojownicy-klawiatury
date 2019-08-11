9e9db6bfe0efd51130b463f86974d887d3864002
Mikołaj Krawczak <mikolak25@gmail.com>
2019-08-11 18:29:24 +0200

    Last changes in text formating

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,10 +1,10 @@
 package com.infoshareacademy;
+
 public class App {
     public static void main(String[] args) {
-        Parser parser = new Parser ( );
-        parser.parseFileToObjects ();
+        Parser parser = new Parser();
+        parser.parseFileToObjects();
         Menu menu = new Menu();
         menu.menu();
-
     }
 }
--- src/main/java/com/infoshareacademy/Parser.java
+++ src/main/java/com/infoshareacademy/Parser.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -8,17 +10,19 @@ import java.util.Objects;
 
 class Parser {
     private static List<Book> books;
+
     public void parseFileToObjects() {
-        ObjectMapper mapper = new ObjectMapper ( );
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            books = mapper.readValue (new File (Objects.requireNonNull (getClass ( ).getClassLoader ( ).getResource ("books.json")).getFile ( )), new TypeReference<List<Book>> ( ) {
+            books = mapper.readValue(new File(Objects.requireNonNull(getClass().getClassLoader().getResource("books.json")).getFile()), new TypeReference<List<Book>>() {
             });
         } catch (IOException e) {
-            System.out.println ("Problem with IO occurred");
+            System.out.println("Problem with IO occurred");
         } catch (NullPointerException n) {
-            System.out.println ("Problem with NullPointerException");
+            System.out.println("Problem with NullPointerException");
         }
     }
+
     public static List<Book> getBooks() {
         return books;
     }
