83cca92a32ae33217e47a4c1276c1de335036513
Robert <lisrobert0023@gmail.com>
2019-08-14 15:43:06 +0200

    Handled possible exceptions with Config class

--- src/main/java/com/infoshareacademy/Config.java
+++ src/main/java/com/infoshareacademy/Config.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-    public void loadConfig() throws IOException {
-        Properties config = new Properties();
-        FileInputStream fileInputStream= new FileInputStream("config.properties");
-        config.load(fileInputStream);
-
+    public void loadConfig(String property) {
+        try {
+            Properties config = new Properties();
+            FileInputStream fileInputStream = new FileInputStream("config.properties");
+            config.load(fileInputStream);
+            config.getProperty(property);
+        } catch (IOException e) {
+            System.out.println("Problem with IO occurred");
+        } catch (NullPointerException n) {
+            System.out.println("Problem with NullPointerException");
+        }
     }
-
 }
