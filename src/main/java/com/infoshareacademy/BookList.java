65bda0ddc47b4913cdffc414bebd9985386c384f
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-08-19 23:21:53 +0200

    Working BookSearcher added.

--- src/main/java/com/infoshareacademy/BookList.java
+++ src/main/java/com/infoshareacademy/BookList.java
@@ -6,36 +6,40 @@ import java.util.List;
 import static com.infoshareacademy.Parser.*;
 
 class BookList {
-    void listBooks() {
-        Scanner scanner = new Scanner(System.in);
-        List<Book> books = getBooks();
-        int lines = 20;
-        int bookCounter = 0;
-        int currentLine;
-        int currentPage = 0;
-        String nextPageCheck;
-        do {
-            if (currentPage > 0) {
-                System.out.println("Enter- Kontynuuj wyświetlanie Z- Zakończ W- wyświetl szczegóły książki ");
-                nextPageCheck = scanner.nextLine();
-                if (nextPageCheck.equals("Z") || nextPageCheck.equals("z")) {
-                    Menu menu = new Menu();
-                    menu.menu();
-                    break;
-                }
-                if ((nextPageCheck.equals("W") || nextPageCheck.equals("w"))) {
-                    SingleBookView singleBookView = new SingleBookView();
-                    singleBookView.selectBook();
-                    break;
-                }
-            }
-            currentLine = 0;
-            do {
-                currentPage++;
-                currentLine++;
-                System.out.println(bookCounter + ". \"" + books.get(bookCounter).getTitle() + "\" - " + books.get(bookCounter).getAuthor());
-                bookCounter++;
-            } while (currentLine < lines && currentPage < books.size());
-        } while (currentPage < books.size());
-    }
+
+  void listBooks() {
+    Scanner scanner = new Scanner(System.in);
+    List<Book> books = getBooks();
+    int lines = 20;
+    int bookCounter = 0;
+    int currentLine;
+    int currentPage = 0;
+    String nextPageCheck;
+    do {
+      if (currentPage > 0) {
+        System.out.println(
+            "\nEnter- Kontynuuj wyświetlanie || Z- Zakończ || W- wyświetl szczegóły książki ");
+        nextPageCheck = scanner.nextLine();
+        if (nextPageCheck.equalsIgnoreCase("Z")) {
+          Menu menu = new Menu();
+          menu.menu();
+          break;
+        }
+        if (nextPageCheck.equalsIgnoreCase("W")) {
+          SingleBookView singleBookView = new SingleBookView();
+          singleBookView.selectBook();
+          break;
+        }
+      }
+      currentLine = 0;
+      do {
+        currentPage++;
+        currentLine++;
+        System.out.println(
+            bookCounter + 1 + ". \"" + books.get(bookCounter).getTitle() + "\" - " + books
+                .get(bookCounter).getAuthor());
+        bookCounter++;
+      } while (currentLine < lines && currentPage < books.size());
+    } while (currentPage < books.size());
+  }
 }
