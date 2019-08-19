e870f625a80ac8b4071873c97d16198c21a8ae94
Robert Lis <lisrobert0023@gmail.com>
2019-08-19 16:43:01 +0200

    Made temporary version of book sorter

--- src/main/java/com/infoshareacademy/BookSorter.java
+++ src/main/java/com/infoshareacademy/BookSorter.java
@@ -1,15 +1,21 @@
 package com.infoshareacademy;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import static com.infoshareacademy.Parser.getBooks;
+
 public class BookSorter {
     private Config config = new Config();
+    private List<Book> books = getBooks();
 
     public void sortByAuthor() {
         config.loadConfig("sortByAuthor");
         String property = config.getProperty();
         if (property.equals("ASC")){
-
+            books.sort(Comparator.comparing(Book::getAuthor));
         } else if (property.equals("DSC")){
-
+            books.sort(Comparator.comparing(Book::getAuthor).reversed());
         }
     }
 
@@ -17,9 +23,9 @@ public class BookSorter {
         config.loadConfig("sortByTitle");
         String property = config.getProperty();
         if (property.equals("ASC")){
-
+            books.sort(Comparator.comparing(Book::getTitle));
         } else if (property.equals("DSC")){
-
+            books.sort(Comparator.comparing(Book::getTitle).reversed());
         }
     }
 }
