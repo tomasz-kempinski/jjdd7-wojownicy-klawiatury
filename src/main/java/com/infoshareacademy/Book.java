8ff0576bc634dfa98abcabb91a51042f9fdd4fb2
Robert Lis <lisrobert0023@gmail.com>
2019-08-20 16:23:18 +0200

    Formatted code

--- src/main/java/com/infoshareacademy/Book.java
+++ src/main/java/com/infoshareacademy/Book.java
@@ -64,12 +64,12 @@ public class Book {
 
   @Override
   public boolean equals(Object o) {
-      if (this == o) {
-          return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-          return false;
-      }
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     Book book = (Book) o;
     return kind.equals(book.kind) &&
         title.equals(book.title) &&
--- src/main/java/com/infoshareacademy/BookSearcher.java
+++ src/main/java/com/infoshareacademy/BookSearcher.java
@@ -1,75 +1,75 @@
 package com.infoshareacademy;
 
+import static com.infoshareacademy.Parser.getBooksTemplate;
+
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import static com.infoshareacademy.Parser.getBooksTemplate;
-
 public class BookSearcher {
 
-    private List<Book> books;
+  private List<Book> books;
 
-    public BookSearcher() {
-        this.books = getBooksTemplate();
+  public BookSearcher() {
+    this.books = getBooksTemplate();
+  }
+
+  public void listBooksFound() {
+    final List<Book> booksFound = findBooks(authorProvider(), titleProvider(), audioProvider());
+    BooksPrinter.printListOfBooks(booksFound);
+  }
+
+  private String authorProvider() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj autora lub pomiń wciskając ENTER:");
+    String author = scanner.nextLine();
+
+    if (author.length() == 0) {
+      return "";
+    } else if (author.length() < 3) {
+      System.out.println("Wpisz co najmniej 3 znaki!");
+      return authorProvider();
+    } else {
+      return author;
     }
+  }
 
-    public void listBooksFound() {
-        final List<Book> booksFound = findBooks(authorProvider(), titleProvider(), audioProvider());
-        BooksPrinter.printListOfBooks(booksFound);
+  private String titleProvider() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj tytuł lub pomiń wciskając ENTER");
+    String title = scanner.nextLine();
+
+    if (title.length() == 0) {
+      return "";
+    } else if (title.length() < 3) {
+      System.out.println("Wpisz co najmniej 3 znaki!");
+      return titleProvider();
+    } else {
+      return title;
     }
+  }
 
-    private String authorProvider() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj autora lub pomiń wciskając ENTER:");
-        String author = scanner.nextLine();
+  private boolean audioProvider() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Czy książka ma mieć wersję audio? (T/N)");
+    String audio = scanner.nextLine();
+    boolean hasAudio = false;
 
-        if (author.length() == 0) {
-            return "";
-        } else if (author.length() < 3) {
-            System.out.println("Wpisz co najmniej 3 znaki!");
-            return authorProvider();
-        } else {
-            return author;
-        }
+    if (!audio.equalsIgnoreCase("T") && !audio.equalsIgnoreCase("N")) {
+      System.out.println("Podaj prawidłowe dane!");
+      audioProvider();
+    } else if (audio.equalsIgnoreCase("T")) {
+      hasAudio = true;
     }
+    return hasAudio;
+  }
 
-    private String titleProvider() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj tytuł lub pomiń wciskając ENTER");
-        String title = scanner.nextLine();
-
-        if (title.length() == 0) {
-            return "";
-        } else if (title.length() < 3) {
-            System.out.println("Wpisz co najmniej 3 znaki!");
-            return titleProvider();
-        } else {
-            return title;
-        }
-    }
-
-    private boolean audioProvider() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Czy książka ma mieć wersję audio? (T/N)");
-        String audio = scanner.nextLine();
-        boolean hasAudio = false;
-
-        if (!audio.equalsIgnoreCase("T") && !audio.equalsIgnoreCase("N")) {
-            System.out.println("Podaj prawidłowe dane!");
-            audioProvider();
-        } else if (audio.equalsIgnoreCase("T")) {
-            hasAudio = true;
-        }
-        return hasAudio;
-    }
-
-    private List<Book> findBooks(String author, String title, Boolean hasAudio) {
-        return books.stream()
-                .filter(
-                        book -> book.getAuthor().toLowerCase().contains(author.toLowerCase()) && book.getTitle()
-                                .toLowerCase().contains(title.toLowerCase())
-                                && book.getHasAudio() == hasAudio)
-                .collect(Collectors.toList());
-    }
+  private List<Book> findBooks(String author, String title, Boolean hasAudio) {
+    return books.stream()
+        .filter(
+            book -> book.getAuthor().toLowerCase().contains(author.toLowerCase()) && book.getTitle()
+                .toLowerCase().contains(title.toLowerCase())
+                && book.getHasAudio() == hasAudio)
+        .collect(Collectors.toList());
+  }
 }
