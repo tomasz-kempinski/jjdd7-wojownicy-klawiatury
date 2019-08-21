2ddaa8d57bd2ef490038af00d05767cd322bf606
Kacper-Rucinski <kacper.rucinski99@outlook.com>
2019-08-21 16:04:42 +0200

    Unnecessary setter deleted from class Book

--- src/main/java/com/infoshareacademy/Book.java
+++ src/main/java/com/infoshareacademy/Book.java
@@ -25,40 +25,20 @@ public class Book {
     return kind;
   }
 
-  public void setKind(String kind) {
-    this.kind = kind;
-  }
-
   public String getTitle() {
     return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
   }
 
   public String getAuthor() {
     return author;
   }
 
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-
   public Boolean getHasAudio() {
     return hasAudio;
   }
 
-  public void setHasAudio(Boolean hasAudio) {
-    this.hasAudio = hasAudio;
-  }
-
   public String getGenre() {
     return genre;
-  }
-
-  public void setGenre(String genre) {
-    this.genre = genre;
   }
 
   @JsonAnyGetter
