1c97be907b9d2c6ba43f28f6ed4ad0a820667e4e
Kacper Ruciński <53050244+Kacper-Rucinski@users.noreply.github.com>
2019-08-22 23:04:35 +0200

    added ScreenCleaner

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 public class App {
 
   public static void main(String[] args) throws FileNotFoundException {
+    new ScreenCleaner().cleanScreen();
     Menu menu = new Menu();
     menu.menu();
   }
