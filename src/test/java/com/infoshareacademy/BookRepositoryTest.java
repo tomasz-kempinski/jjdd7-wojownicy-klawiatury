bbe5504ff02701f2261da1a45d72c1cada3fd711
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-08-24 20:28:05 +0200

    Tests updated

---
+++ src/test/java/com/infoshareacademy/BookRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.infoshareacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BookRepositoryTest {
+
+  Book book1 = new Book("drama", "Romeo and Juliet", "William Shakespeare", true, "tragedy");
+  Book book2 = new Book("epic", "The Lord Of The Rings", "J. R. R. Tolkien", false, "fantasy");
+
+  @Test
+  void getBookRepository_isNotEmpty() {
+
+    BookRepository.bookRepository.add(book1);
+    BookRepository.bookRepository.add(book2);
+
+    List<Book> output = BookRepository.getBookRepository();
+
+    assertThat(output).hasSize(2)
+        .isNotEmpty()
+        .contains(book1, book2);
+  }
+}
