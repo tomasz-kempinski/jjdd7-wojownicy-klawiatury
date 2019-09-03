9febfeead739b068defe649efdf74910ec386e45
Mikołaj Krawczak <mikolak25@gmail.com>
2019-09-03 21:27:19 +0200

    Logs ready

--- web/src/main/Java/com/infoshareacademy/wojownicy/servlet/LogTestServlet.java
+++ web/src/main/Java/com/infoshareacademy/wojownicy/servlet/LogTestServlet.java
@@ -1,5 +1,30 @@
 package com.infoshareacademy.wojownicy.servlet;
 
-public class LogTestServlet {
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/log-test-servlet")
+public class LogTestServlet extends HttpServlet {
+  private final Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.setContentType("text/html;charset=utf-8");
+    PrintWriter printWriter = resp.getWriter();
+    printWriter.println("Logs Added to file and console");
+    logger.debug("DEBUG");
+    logger.trace("TRACE");
+    logger.info("INFO");
+    logger.warn("WARN");
+    logger.error("ERROR");
+  }
 }
