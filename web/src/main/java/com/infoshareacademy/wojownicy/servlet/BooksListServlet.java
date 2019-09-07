108b15ec258d88740f688c6d94939fe72978c5ed
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-09-07 08:56:22 +0200

    Add table template and servlet for books list

---
+++ web/src/main/java/com/infoshareacademy/wojownicy/servlet/BooksListServlet.java
@@ -0,0 +1,41 @@
+package com.infoshareacademy.wojownicy.servlet;
+
+import com.infoshareacademy.wojownicy.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/book-list")
+public class BooksListServlet extends HttpServlet {
+
+  @Inject
+  private TemplateProvider templateProvider;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    resp.setContentType("text/html; charset=UTF-8");
+
+    Template template = templateProvider.getTemplate(getServletContext(), "book-list.ftlh");
+
+    Map<String, Object> dataModel = new HashMap<>();
+
+    PrintWriter printWriter = resp.getWriter();
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
+}
