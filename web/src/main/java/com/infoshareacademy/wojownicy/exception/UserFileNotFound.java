976e24cb99e60263cd5341dce020de0e8ee2a66b
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-09-09 16:54:25 +0200

    Add FileUploadProcessor and UserFileNotFound exception

---
+++ web/src/main/java/com/infoshareacademy/wojownicy/exception/UserFileNotFound.java
@@ -0,0 +1,8 @@
+package com.infoshareacademy.wojownicy.exception;
+
+public class UserFileNotFound extends Exception {
+
+    public UserFileNotFound(){}
+
+    public UserFileNotFound(String message) {super(message);}
+}
