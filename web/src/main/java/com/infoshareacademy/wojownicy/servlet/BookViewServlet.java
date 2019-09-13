99890ad4cb80adacef1e1c87a64f18ce117ee8f9
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-09-13 16:47:48 +0200

    Add Book view template and servlet

---
+++ web/src/main/java/com/infoshareacademy/wojownicy/servlet/BookViewServlet.java
@@ -0,0 +1,48 @@
+package com.infoshareacademy.wojownicy.servlet;
+
+import com.infoshareacademy.wojownicy.dao.BookDaoBean;
+import com.infoshareacademy.wojownicy.domain.entity.Book;
+import com.infoshareacademy.wojownicy.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/book-view")
+public class BookViewServlet extends HttpServlet {
+
+  @Inject
+  private TemplateProvider templateProvider;
+  
+  @Inject
+  private BookDaoBean bookDaoBean;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+
+    resp.setContentType("text/html; charset=UTF-8");
+
+    Template template = templateProvider.getTemplate(getServletContext(), "book-view.ftlh");
+
+    Map<String, List<Book>> dataModel = new HashMap<>();
+
+    PrintWriter printWriter = resp.getWriter();
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
+}
