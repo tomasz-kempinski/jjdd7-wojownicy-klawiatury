1a55ab8e83a19e6a1d482f137d0357035befb4dd
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2019-09-14 19:27:36 +0200

    Change encoding filter

--- web/src/main/java/com/infoshareacademy/wojownicy/filter/EncodingFilter.java
+++ web/src/main/java/com/infoshareacademy/wojownicy/filter/EncodingFilter.java
@@ -7,16 +7,24 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebFilter(
     filterName = "EncodingFilter",
-    urlPatterns = {"/*"})
+    urlPatterns = {"/*"}
+)
 public class EncodingFilter implements Filter {
+
+  private Logger logger = LoggerFactory.getLogger(EncodingFilter.class.getName());
+
+  private final String codingType = "UTF-8";
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response,
       FilterChain filterChain) throws IOException, ServletException {
-    response.setContentType("text/html; charset=UTF-8");
+    logger.info("Coding type is set");
+    response.setCharacterEncoding(codingType);
     filterChain.doFilter(request, response);
   }
 }
