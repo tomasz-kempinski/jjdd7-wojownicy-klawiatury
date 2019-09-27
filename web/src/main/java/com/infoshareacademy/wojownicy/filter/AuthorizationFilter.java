dbfa559110d7915811bfb00dfad9443c61b4f333
Robert <lisrobert0023@gmail.com>
2019-09-27 18:35:23 +0200

    Changed standalone path

---
+++ web/src/main/java/com/infoshareacademy/wojownicy/filter/AuthorizationFilter.java
@@ -0,0 +1,44 @@
+package com.infoshareacademy.wojownicy.filter;
+
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter(
+    filterName = "AuthorizationFilter",
+    urlPatterns = {"/home", "/"}
+)
+public class AuthorizationFilter implements Filter {
+
+  private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+
+    HttpServletRequest req = (HttpServletRequest) servletRequest;
+    HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+    Optional<Object> userType = Optional.ofNullable(req.getSession().getAttribute("userType"));
+logger.info("user type is: " + userType.toString());
+
+    if (userType.isEmpty()) {
+      req.setAttribute("siteType", "-guest");
+    } else if (userType.get().equals("admin") || userType.get().equals("superadmin")) {
+      req.setAttribute("siteType", "-admin");
+    } else if (userType.get().equals("user")){
+      req.setAttribute("siteType", "-user");
+    }
+
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+}
