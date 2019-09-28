cc16be5d4842bcbfe9bf9775d8987ab0ac777d87
Robert <lisrobert0023@gmail.com>
2019-09-28 12:36:19 +0200

    Created some js

---
+++ web/src/main/webapp/js/permissions-button-switcher.js
@@ -0,0 +1,17 @@
+// $(function () {
+//   $(document).ready(function () {
+//     $.ajax({
+//       url: '/api/user/get-permission/{id}',
+//       type: 'GET',
+//       success: function (result) {
+//
+//         $("#give").show();
+//         $("#revoke").hide();
+//       },
+//       error: function (event) {
+//         $("#give").show();
+//         $("#revoke").hide();
+//       }
+//     });
+//   });
+// });
